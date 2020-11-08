10147cb9bd6cb75fd0b435af27ab8ea24aa5d194
Pradeep Vasudevan <pvasudev@mathworks.com>
2020-11-08 15:18:36 -0500

    MPS Ref Arch AWS Health Check Test

---
+++ healthcheck/README.md
@@ -0,0 +1,3 @@
+# MPS_RefArch_AWS_HealthCheck
+This section of repository is to contain the tests for health check of published MATLAB Production Server Ref Arch AWS health check.
+It will be scheduled to run everyday and confirm that the deployment can be done successfully and links that we have published to github are in good health.
---
+++ healthcheck/test_mpsrefarch_HealthCheck.py
@@ -0,0 +1,27 @@
+import refarch_testtools.deploy as deploy
+import sys
+
+def main(keypairname, password, ipAddress):
+    parameters = [{'ParameterKey': 'KeyPairName', 'ParameterValue': keypairname},
+                  {'ParameterKey': 'ClientIPAddress', 'ParameterValue': ipAddress},
+                  {'ParameterKey': 'WorkerSystem', 'ParameterValue': 'Ubuntu'}, 
+                  {'ParameterKey': 'Password', 'ParameterValue': password},
+                  {'ParameterKey': 'ConfirmPassword', 'ParameterValue': password}]
+
+    template_url = "https://matlab-production-server-templates.s3.amazonaws.com/MatlabProductionServer_R2020a_New.yml"
+    
+    try:
+        print("deploying the stack")
+        stack = deploy.deploy_stack(template_url, parameters, "us-east-1", "mpsRefArchHealthCheck")
+        print(stack)
+        print("success deploying the stack")
+    except Exception as e:
+        raise (e)
+   
+    # delete the deployment
+    print("deleting the stack")
+    deploy.delete_stack(stack)
+    print("success deleting the stack")
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
