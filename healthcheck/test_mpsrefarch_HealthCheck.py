2d277d7a5eb8fd81d940230cbdfa73ddb4f54c95
Pradeep Vasudevan <pvasudev@mathworks.com>
2021-02-17 16:50:40 -0500

    MATLAB Production Server Ref Arch AWS HealthCheck Latest 2 releases

--- healthcheck/test_mpsrefarch_HealthCheck.py
+++ healthcheck/test_mpsrefarch_HealthCheck.py
@@ -1,27 +1,49 @@
 import refarch_testtools.deploy as deploy
 import sys
+import re
+import requests
+import datetime
+import urllib
+import random
+from datetime import date
 
 def main(keypairname, password, ipAddress):
+    # Reference architectures in production.
+    ref_arch_name = 'matlab-production-server-on-aws'
     parameters = [{'ParameterKey': 'KeyPairName', 'ParameterValue': keypairname},
                   {'ParameterKey': 'ClientIPAddress', 'ParameterValue': ipAddress},
                   {'ParameterKey': 'WorkerSystem', 'ParameterValue': 'Ubuntu'}, 
                   {'ParameterKey': 'Password', 'ParameterValue': password},
                   {'ParameterKey': 'ConfirmPassword', 'ParameterValue': password}]
 
-    template_url = "https://matlab-production-server-templates.s3.amazonaws.com/r2020b_mps_refarch/MatlabProductionServer_New.yml"
+    # Find latest MATLAB release from Github page and get template json path.
+    res = requests.get(f"https://github.com/mathworks-ref-arch/{ref_arch_name}/blob/master/releases/")
     
-    try:
-        print("deploying the stack")
-        stack = deploy.deploy_stack(template_url, parameters, "us-east-1", "mpsRefArchAuto")
+    latest_releases = [re.findall("master/releases/(R\d{4}[ab]\\b)", res.text)[-1], re.findall("master/releases/(R\d{4}[ab]\\b)", res.text)[-2]]
+    for i in range(2):
+        matlab_release = latest_releases[i]
+        print("Testing Health Check Release: " + matlab_release + "\n")
+        github_base_dir = "https://raw.githubusercontent.com/mathworks-ref-arch"
+        template_url_path = f"{github_base_dir}/{ref_arch_name}/master/releases/{matlab_release}/templates/templateURL.txt"
+        file = urllib.request.urlopen(template_url_path)
+        template_url = file.readline().decode("utf-8").rstrip()
+        
+        stack_name = "mps-refarch-health-check-" + matlab_release + date.today().strftime('%m-%d-%Y') + str(random.randint(1,101))
+        ct = datetime.datetime.now()
+        print("Date time before deployment of stack:-", ct)
 
-        print("success deploying the stack")
-    except Exception as e:
-        raise (e)
-   
-    # delete the deployment
-    print("deleting the stack")
-    deploy.delete_stack(stack)
-    print("success deleting the stack")
+        try:
+            print("deploying the stack")
+            stack = deploy.deploy_stack(template_url, parameters, "us-east-1", stack_name)
+            print("success deploying the stack")
+        except Exception as e:
+            raise (e)
+        # delete the deployment
+        print("deleting the stack")
+        deploy.delete_stack(stack)
+        print("success deleting the stack")
+        ct = datetime.datetime.now()
+        print("Date time after deployment and deletion of stack:-", ct)
 
 if __name__ == '__main__':
     main(sys.argv[1], sys.argv[2], sys.argv[3])
